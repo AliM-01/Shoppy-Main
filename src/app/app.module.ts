82cecd286f5f8943384282e404b9f06243cc1d6b
Ali M <alimousavi-01@outlook.com>
2021-11-22 18:48:19 +0330

    Created IndexComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_components/header/header.component';
 import { MenuComponent } from './_components/menu/menu.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
