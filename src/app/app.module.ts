f6eb2ee049fb6874c05d45209d7a609666b57fb6
Ali M <alimousavi-01@outlook.com>
2021-11-22 18:49:52 +0330

    Created FooterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './_components/header/header.component';
 import { MenuComponent } from './_components/menu/menu.component';
 import { IndexComponent } from './pages/index/index.component';
+import { FooterComponent } from './_components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    IndexComponent
+    IndexComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
