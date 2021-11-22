efd316a82cce40b539038e6e10948f0a56484ecc
Ali M <alimousavi-01@outlook.com>
2021-11-22 18:56:22 +0330

    Created ComponentsModule

---
+++ src/app/_components/components.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+  ],
+  schemas: []
+})
+export class ComponentsModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './_components/header/header.component';
-import { MenuComponent } from './_components/menu/menu.component';
+import { ComponentsModule } from './_components/components.module';
 import { IndexComponent } from './pages/index/index.component';
-import { FooterComponent } from './_components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    IndexComponent,
-    FooterComponent
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
