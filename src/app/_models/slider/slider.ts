fc49331abf6b9d4fe5dfb6dcd4150092e20d4827
Ali M <alimousavi-01@outlook.com>
2021-11-23 12:51:12 +0330

    Created SliderModel

---
+++ src/app/_models/slider/slider.ts
@@ -0,0 +1,12 @@
+export class SliderModel {
+    constructor(
+         public id: number,
+         public heading: string,
+         public text: string,
+         public imagePath: string,
+         public imageAlt: string,
+         public imageTitle: string,
+         public btnLink: string,
+         public btnText: string
+     ){}
+ }
