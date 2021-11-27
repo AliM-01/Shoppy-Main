4389895c23fb310636e2f3c3778ffadb7e2e9a43
Ali M <alimousavi-01@outlook.com>
2021-11-27 19:37:22 +0330

    UI Bug Fixes

--- src/app/_components/components.module.ts
+++ src/app/_components/components.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
 import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    MenuComponent,
     FooterComponent,
     MobileHeaderComponent
   ],
@@ -19,7 +17,6 @@ import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
   ],
   exports: [
     HeaderComponent,
-    MenuComponent,
     FooterComponent,
     MobileHeaderComponent
   ],
