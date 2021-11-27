d4a147cf45f50a674385f050a03e71910ce3f1e6
Ali M <alimousavi-01@outlook.com>
2021-11-27 20:27:19 +0330

    Added productCategoryBaseImagePath In Environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   apiUrl: "https://localhost:5001/api",
   sliderBaseImagePath: "https://localhost:5001/slider",
+  productCategoryBaseImagePath: "https://localhost:5001/product-category",
 };
