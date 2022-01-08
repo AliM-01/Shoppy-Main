d80ccc4c301b07d63152ebf8cd8d49c80ed3e1de
Ali M <alimousavi-01@outlook.com>
2022-01-08 14:42:33 +0330

    Refactor ProductCategoryService: Rxjs Operators

--- src/app/_services/shop/product-category/product-category.service.ts
+++ src/app/_services/shop/product-category/product-category.service.ts
@@ -1,19 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { IResponse } from '@app_models/common/IResponse';
 import { ProductCategoryModel } from '@app_models/shop/product-category/product-category';
 import { environment } from '@environments/environment';
+import { ToastrService } from 'ngx-toastr';
+import { LoadingService } from '@loading';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductCategoryService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private loading: LoadingService
   ) { }
 
   getProductCategorysList(): Observable<IResponse<ProductCategoryModel[]>> {
-    return this.http.get<IResponse<ProductCategoryModel[]>>(`${environment.shopBaseApiUrl}/product-category/get-list`);
+    this.loading.loadingOn();
+    return this.http.get<IResponse<ProductCategoryModel[]>>
+    (`${environment.shopBaseApiUrl}/product-category/get-list`)
+    .pipe(
+      tap(() => this.loading.loadingOff()),
+      catchError((error: HttpErrorResponse) => {
+
+        this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+        this.loading.loadingOff();
+
+        return throwError(error);
+      })
+    );
   }
 }
