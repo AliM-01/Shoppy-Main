32715e77fe46596d3c7ae04e6eede4f8130abee7
Ali M <alimousavi-01@outlook.com>
2022-01-08 14:43:38 +0330

    Refactor SliderService: Rxjs Operators

--- src/app/_services/shop/slider/slider.service.ts
+++ src/app/_services/shop/slider/slider.service.ts
@@ -1,19 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { IResponse } from '@app_models/common/IResponse';
 import { SliderModel } from '@app_models/shop/slider/slider';
 import { environment } from '@environments/environment';
+import { LoadingService } from '@loading';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SliderService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private loading: LoadingService
   ) { }
 
   getSlidersList(): Observable<IResponse<SliderModel[]>> {
-    return this.http.get<IResponse<SliderModel[]>>(`${environment.shopBaseApiUrl}/slider/get-list`);
+    this.loading.loadingOn();
+    return this.http.get<IResponse<SliderModel[]>>
+    (`${environment.shopBaseApiUrl}/slider/get-list`)
+    .pipe(
+      tap(() => this.loading.loadingOff()),
+      catchError((error: HttpErrorResponse) => {
+
+        this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+        this.loading.loadingOff();
+
+        return throwError(error);
+      })
+    );
   }
 }
