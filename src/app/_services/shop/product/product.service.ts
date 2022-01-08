ce3c30ebd633ace2a35603b303722868a4898fed
Ali M <alimousavi-01@outlook.com>
2022-01-08 14:41:27 +0330

    Refactor ProductService: Rxjs Operators

--- src/app/_services/shop/product/product.service.ts
+++ src/app/_services/shop/product/product.service.ts
@@ -1,19 +1,37 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IResponse } from '@app_models/common/IResponse';
 import { ProductModel } from '@app_models/shop/product/product';
 import { environment } from '@environments/environment';
-import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, throwError } from 'rxjs';
+import { LoadingService } from '@loading';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private loading: LoadingService,
   ) { }
 
   getLatestProducts(): Observable<IResponse<ProductModel[]>> {
-    return this.http.get<IResponse<ProductModel[]>>(`${environment.shopBaseApiUrl}/product/get-latest`);
+    this.loading.loadingOn();
+
+    return this.http.get<IResponse<ProductModel[]>>
+    (`${environment.shopBaseApiUrl}/product/get-latest`)
+    .pipe(
+      tap(() => this.loading.loadingOff()),
+      catchError((error: HttpErrorResponse) => {
+
+        this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+        this.loading.loadingOff();
+
+        return throwError(error);
+      })
+    );
   }
 }
