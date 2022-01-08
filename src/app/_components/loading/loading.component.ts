7c6ddb4f143ef4a25cdb6624b0756097fc1ea05d
Ali M <alimousavi-01@outlook.com>
2022-01-08 14:39:01 +0330

    Bug Fixes

--- src/app/_components/loading/loading.component.ts
+++ src/app/_components/loading/loading.component.ts
@@ -4,7 +4,7 @@ import { LoadingService } from '@loading';
 @Component({
   selector: 'app-loading',
   template: `
-    <ngx-loading [show]="(loadingService.loading$ | async)"></ngx-loading>
+    <ngx-loading [show]="!!(loadingService.loading$ | async)"></ngx-loading>
   `
 })
 export class LoadingComponent implements OnInit {
