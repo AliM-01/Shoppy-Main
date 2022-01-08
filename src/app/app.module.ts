e4f241b45829322f9ea0ca2d009ba5e34c18dbb5
Ali M <alimousavi-01@outlook.com>
2022-01-08 14:36:53 +0330

    Added ToastrModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexModule } from './pages/index/index.module';
 import { ComponentsModule } from './_components/components.module';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,11 @@ import { ComponentsModule } from './_components/components.module';
     AppRoutingModule,
     HttpClientModule,
     ComponentsModule,
-    IndexModule
+    IndexModule,
+    ToastrModule.forRoot({
+      tapToDismiss: false,
+      autoDismiss: true
+    }),
   ],
   providers: [SliderService, ProductCategoryService, ProductService],
   bootstrap: [AppComponent]
