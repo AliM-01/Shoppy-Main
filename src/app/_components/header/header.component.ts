70f04b5a722e5d1ad1b7fcfd40cc6a559413cc80
Ali M <alimousavi-01@outlook.com>
2022-01-12 11:29:18 +0330

    Refactor Header Menu

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductCategoryModel } from '@app_models/shop/product-category/product-category';
+import { ProductCategoryService } from '@app_services/shop/product-category/product-category.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  
+  productCategories: ProductCategoryModel[] = [];
+  
+  constructor(
+    private productCategoryService: ProductCategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.productCategoryService.getProductCategoriesList().subscribe(res => {
+      if(res.status ==="success"){
+        this.productCategories = res.data;
+      }
+    });
   }
 
 }
