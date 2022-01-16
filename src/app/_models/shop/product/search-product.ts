c4302aae83ab69fa2c9ee54aff700d3020e594c2
Ali M <alimousavi-01@outlook.com>
2022-01-16 14:16:34 +0330

    Created SearchProductModel

---
+++ src/app/_models/shop/product/search-product.ts
@@ -0,0 +1,50 @@
+import { IPaging, PagingDataSortCreationDateOrder, PagingDataSortIdOrder } from "@app_models/common/IPaging";
+import { ProductModel } from "./product";
+
+export class SearchProductModel implements IPaging {
+    pageId: number;
+    allPagesCount: number;
+    takePage: number;
+    sortCreationDateOrder: PagingDataSortCreationDateOrder = PagingDataSortCreationDateOrder.DES;
+    sortIdOrder: PagingDataSortIdOrder;
+
+    selectedCategories: string[]
+    phrase: string;
+    categoryId: string;
+    products: ProductModel[];
+    searchProductPriceOrder: SearchProductPriceOrder = SearchProductPriceOrder.All;
+    filterMinPrice: number;
+    filterMaxPrice: number;
+    selectedMinPrice: number;
+    selectedMaxPrice: number;
+
+    constructor(
+        phrase: string,
+        categoryId: string,
+        products: ProductModel[],
+        pageId: number,
+        takePage: number,
+        sortCreationDateOrder: PagingDataSortCreationDateOrder,
+        searchProductPriceOrder: SearchProductPriceOrder,
+        selectedMinPrice: number,
+        selectedMaxPrice: number
+        ){
+        this.phrase = phrase;
+        this.categoryId = categoryId;
+        this.products = products;
+        this.pageId = pageId;
+        this.takePage = takePage;
+        this.sortCreationDateOrder = sortCreationDateOrder;
+        this.searchProductPriceOrder = searchProductPriceOrder;
+        this.selectedMinPrice = selectedMinPrice;
+        this.selectedMaxPrice = selectedMaxPrice;
+        
+    }
+}
+
+export enum SearchProductPriceOrder
+{
+    All,
+    Price_Des,
+    Price_Asc
+}
