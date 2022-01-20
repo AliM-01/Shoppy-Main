ef950a02fb3520e533f34194ba4fc847e4dbf044
Ali M <alimousavi-01@outlook.com>
2022-01-20 15:34:06 +0330

    Implemented ProductDetailsModel

--- src/app/pages/product/product-details/product-details.page.ts
+++ src/app/pages/product/product-details/product-details.page.ts
@@ -1,6 +1,10 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '@app_services/shop/product/product.service';
+import { environment } from '@environments/environment';
+import { ProductDetailsModel } from '../../../_models/shop/product/product-details';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'product-details',
@@ -8,15 +12,40 @@ import { ProductService } from '@app_services/shop/product/product.service';
 })
 export class ProductDetailsPage implements OnInit {
 
-  
+  product!: ProductDetailsModel;
+  private productTitleSubject: BehaviorSubject<string> = new BehaviorSubject<string>("");
+  productTitle$: Observable<string> = this.productTitleSubject.asObservable();
+  isDataLoaded: boolean = false;
+
+  baseProductPath: string = environment.productBaseImagePath + '/original/';
+  baseProductPicturePath: string = environment.productPictureBaseImagePath + '/thumbnail/';
+
   constructor(
     private productService: ProductService,
+    private _location: Location,
     private router: Router,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    
+    this.isDataLoaded = false;
+    this.activatedRoute.params.subscribe(params => {
+      const slug = params.slug;
+
+      if (slug !== undefined) {
+        this.productService.getProductDetails(slug)
+          .subscribe((res) => {
+            if (res.status === 'success') {
+              this.productTitleSubject.next(res.data.title)
+              this.product = res.data;
+              this.isDataLoaded = true;
+
+            }
+          }, () => this._location.back()
+          );
+      }
+
+    });
 
   }
 }
