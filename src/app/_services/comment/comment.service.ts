8e86d620aae2aa52daa2ac5ab00fae73b8af930a
Ali M <alimousavi-01@outlook.com>
2022-01-31 21:09:11 +0330

    implemented getRecordCommentsById service

--- src/app/_services/comment/comment.service.ts
+++ src/app/_services/comment/comment.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { LoadingService } from '@loading';
+import { CommentModel } from '@app_models/comment/comment';
+import { IResponse } from '@app_models/_common/IResponse';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { environment } from '@environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +17,21 @@ export class CommentService {
     private toastr: ToastrService,
     private loading: LoadingService,
   ) { }
+  
+  getRecordCommentsById(recordId: number): Observable<IResponse<CommentModel[]>> {
+    this.loading.loadingOn();
 
+    return this.http.get<IResponse<CommentModel[]>>
+      (`${environment.commentBaseApiUrl}/get-comments/${recordId}`)
+      .pipe(
+        tap(() => this.loading.loadingOff()),
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+          this.loading.loadingOff();
+
+          return throwError(error);
+        })
+      );
+  }
 }
