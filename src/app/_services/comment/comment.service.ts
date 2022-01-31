b6493fa2044f4a3578bce8aee3bfd5e9f7638b5f
Ali M <alimousavi-01@outlook.com>
2022-01-31 21:12:17 +0330

    implemented addComment service

--- src/app/_services/comment/comment.service.ts
+++ src/app/_services/comment/comment.service.ts
@@ -7,6 +7,7 @@ import { IResponse } from '@app_models/_common/IResponse';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { environment } from '@environments/environment';
+import { AddCommentModel } from '../../_models/comment/add-comment';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,35 @@ export class CommentService {
       (`${environment.commentBaseApiUrl}/get-comments/${recordId}`)
       .pipe(
         tap(() => this.loading.loadingOff()),
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+          this.loading.loadingOff();
+
+          return throwError(error);
+        })
+      );
+  }
+
+  addComment(addCommentData: AddCommentModel): Observable<IResponse<any>> {
+    this.loading.loadingOn();
+
+    const formData = new FormData();
+
+    formData.append('name', addCommentData.name);
+    formData.append('email', addCommentData.email);
+    formData.append('text', addCommentData.text);
+    formData.append('type', addCommentData.type.toString());
+    formData.append('ownerRecordId', addCommentData.ownerRecordId.toString());
+    formData.append('parentId', addCommentData.parentId.toString());
+
+    return this.http.post<IResponse<any>>
+      (`${environment.commentBaseApiUrl}/add`, formData)
+      .pipe(
+        tap((res) => {
+          this.toastr.success(res.message, 'موفقیت', { timeOut: 1500 });
+          this.loading.loadingOff()
+        }),
         catchError((error: HttpErrorResponse) => {
 
           this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
