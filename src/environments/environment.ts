bfb95dbb7525b3c777428e29a0ee0e6ebeb99f10
Ali M <alimousavi-01@outlook.com>
2022-01-31 21:08:32 +0330

    added commentBaseApiUrl to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
   shopBaseApiUrl: "https://localhost:5001/api/shop",
+  commentBaseApiUrl: "https://localhost:5001/api/comment",
+
   sliderBaseImagePath: "https://localhost:5001/slider",
   productCategoryBaseImagePath: "https://localhost:5001/product_category",
   productBaseImagePath: "https://localhost:5001/product",
