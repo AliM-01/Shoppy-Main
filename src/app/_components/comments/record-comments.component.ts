71e3c8e4ce48633d076cd35ad2192ee9b3a96487
Ali M <alimousavi-01@outlook.com>
2022-02-01 14:50:52 +0330

    initialized RecordCommentsComponent

---
+++ src/app/_components/comments/record-comments.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommentService } from '@app_services/comment/comment.service';
+import { CommentModel } from '@app_models/comment/comment';
+import { LoadingService } from '@loading';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'record-comments',
+  templateUrl: './record-comments.component.html',
+  providers: [CommentService]
+})
+export class RecordCommentsComponent implements OnInit {
+
+  @Input() recordId: number;
+  comments: CommentModel[] = [];
+
+  constructor(
+    private commentService: CommentService,
+    private loading: LoadingService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loading.loadingOn();
+
+    this.commentService.getRecordCommentsById(this.recordId).subscribe(res => {
+      this.comments = res.data;
+    });
+
+    this.loading.loadingOff();
+  }
+
+}
