7d368e54d26f48b4fb35b8f1a4ed5f070a4992d6
Ali M <alimousavi-01@outlook.com>
2022-02-17 16:52:20 +0330

    Created ArticleModel

---
+++ src/app/_models/blog/article/article.ts
@@ -0,0 +1,12 @@
+export class ArticleModel {
+    id: number;
+    title: string;
+    summary: string;
+    imagePath: string;
+    imageAlt: string;
+    imageTitle: string;
+    creationDate: string;
+    category: string;
+    categoryId: string;
+    slug: string
+}
