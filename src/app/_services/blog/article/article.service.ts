391d0bdbad299f18adc86ad0bb771bcbcd70a590
Ali M <alimousavi-01@outlook.com>
2022-02-17 18:36:22 +0330

    created ArticleService

---
+++ src/app/_services/blog/article/article.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IResponse } from '@app_models/_common/IResponse';
+import { environment } from '@environments/environment';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, throwError } from 'rxjs';
+import { LoadingService } from '@loading';
+import { catchError, tap } from 'rxjs/operators';
+import { ArticleModel } from '@app_models/blog/article/article';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private loading: LoadingService,
+  ) { }
+
+  getLatestArticles(): Observable<IResponse<ArticleModel[]>> {
+    this.loading.loadingOn();
+
+    return this.http.get<IResponse<ArticleModel[]>>
+      (`${environment.blogBaseApiUrl}/article/get-latest`)
+      .pipe(
+        tap(() => this.loading.loadingOff()),
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+          this.loading.loadingOff();
+
+          return throwError(error);
+        })
+      );
+  }
+
+}
