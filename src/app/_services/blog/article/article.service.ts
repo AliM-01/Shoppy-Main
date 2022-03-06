69f53f2d17dd3cef817e4915b946104acd5c459a
Ali M <alimousavi-01@outlook.com>
2022-03-06 17:52:05 +0330

    Implemented searchArticle service

--- src/app/_services/blog/article/article.service.ts
+++ src/app/_services/blog/article/article.service.ts
@@ -8,6 +8,7 @@ import { LoadingService } from '@loading';
 import { catchError, tap } from 'rxjs/operators';
 import { ArticleModel } from '@app_models/blog/article/article';
 import { ArticleDetailsModel } from '../../../_models/blog/article/article-details';
+import { SearchArticleModel } from '@app_models/blog/article/search-article';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,40 @@ export class ArticleService {
     private toastr: ToastrService,
     private loading: LoadingService,
   ) { }
+
+  searchArticle(search: SearchArticleModel): Observable<IResponse<SearchArticleModel>> {
+
+    this.loading.loadingOn();
+
+    let params = new HttpParams()
+      .set('PageId', search.pageId.toString())
+      .set('TakePage', search.takePage.toString());
+
+    if(search.phrase !== "" && search.phrase !== undefined){
+      params.set('Phrase', search.phrase);
+    }
+
+    if (search.selectedCategories !== null && search.selectedCategories?.length) {
+      for (let category of search.selectedCategories) {
+        params = params.append('SelectedCategories', category);
+      }
+    } else {
+      search.selectedCategories = [];
+    }
+
+    return this.http.get<IResponse<SearchArticleModel>>
+      (`${environment.blogBaseApiUrl}/article/search`, { params })
+      .pipe(
+        tap(() => this.loading.loadingOff()),
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+          this.loading.loadingOff();
+
+          return throwError(error);
+        })
+      );
+  }
 
   getLatestArticles(): Observable<IResponse<ArticleModel[]>> {
     this.loading.loadingOn();
