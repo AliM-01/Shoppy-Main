dcd0ba518b8113f200fb32d10ac225bffb25a9d6
Ali M <alimousavi-01@outlook.com>
2022-03-08 13:21:14 +0330

    Order Service Refactor

--- src/app/_services/order/order.service.ts
+++ src/app/_services/order/order.service.ts
@@ -3,20 +3,20 @@ import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { LoadingService } from '@loading';
 import { CookieService } from 'ngx-cookie-service'
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { CartItemCookieModel } from '@app_models/order/cart-item-cookie';
 
 export const CART_ITEMS_COOKIE_NAME: string = 'cart_items';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'any',
 })
 export class OrderService {
 
   private itemsCountSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   itemsCount$: Observable<number> = this.itemsCountSubject.asObservable();
 
-  cartItemsSubject: BehaviorSubject<Array<CartItemCookieModel>> = new BehaviorSubject([]);
+  private cartItemsSubject: BehaviorSubject<Array<CartItemCookieModel>> = new BehaviorSubject([]);
   cartItems$: Observable<Array<CartItemCookieModel>> = this.cartItemsSubject.asObservable();
 
   constructor(
@@ -24,58 +24,82 @@ export class OrderService {
     private cookieService: CookieService,
     private toastr: ToastrService,
     private loading: LoadingService,
-  ) { }
+  ) {
+    this.loadCart();
+  }
 
 
   addToCart(item: CartItemCookieModel) {
+    console.log('addd');
+
     this.loading.loadingOn();
 
     //-----check if there are items already added in cart
     let existingItems: CartItemCookieModel[] = [];
+
     if (this.cookieService.check(CART_ITEMS_COOKIE_NAME)) {
-      //----- update by adding new items
       existingItems = JSON.parse(this.cookieService.get(CART_ITEMS_COOKIE_NAME));
-      existingItems = [item, ...existingItems];
-      console.log('Items exists');
+
+      if (existingItems !== null) {
+        console.log('Items exists');
+
+        const currentProductInCart: CartItemCookieModel = existingItems.find(x => x.productId === item.productId);
+        if (currentProductInCart !== undefined) {
+          currentProductInCart.count = currentProductInCart.count + item.count;
+
+          const index = existingItems.findIndex((o) => o.productId === currentProductInCart.productId);
+
+          if (index > -1) {
+            existingItems.splice(index, 1);
+          }
+
+          existingItems = [currentProductInCart, ...existingItems]
+
+        } else {
+          existingItems = [item, ...existingItems]
+        }
+      } else {
+        existingItems = [item];
+      }
     }
-    //-----if no items, add new items
     else {
       console.log('NO items exists');
       existingItems = [item];
     }
 
-    const currentProduct: CartItemCookieModel = existingItems.find(x => x.productId === item.productId);
-    if (currentProduct !== undefined) {
-      existingItems.find(x => x.productId === item.productId).count = currentProduct.count + item.count;
-    }
-
     this.cartItemsSubject.next(existingItems);
+    console.log(this.cartItemsSubject.value);
 
     this.loading.loadingOff();
-    this.syncCart();
+    this.saveCart();
   }
 
   loadCart(): void {
-    let items: CartItemCookieModel[] = JSON.parse(this.cookieService.get(CART_ITEMS_COOKIE_NAME)) ?? [];
-    this.cartItemsSubject.next(items);
+    const itemsInCookie = this.cookieService.get(CART_ITEMS_COOKIE_NAME);
+    if (itemsInCookie !== undefined && itemsInCookie !== '') {
+      const items: CartItemCookieModel[] = JSON.parse(itemsInCookie);
+      console.log('load', items);
+      this.cartItemsSubject.next(items);
+    } else {
+      this.cartItemsSubject.next([]);
+    }
+    this.calculateAllItemsCount();
   }
 
-  syncCart(): void {
-    this.cookieService.set(CART_ITEMS_COOKIE_NAME, JSON.stringify(this.cartItemsSubject.value), 200000)
-    this.loadCart();
+  saveCart(): void {
+    this.cookieService.delete(CART_ITEMS_COOKIE_NAME);
 
-    let count = 0;
-    for (const item of this.cartItemsSubject.value) {
-      count = count + item.count;
+    if (this.cartItemsSubject.value.length > 0) {
+      this.cookieService.set(CART_ITEMS_COOKIE_NAME, JSON.stringify(this.cartItemsSubject.value), 200000)
     }
 
-    this.itemsCountSubject.next(count);
+    this.calculateAllItemsCount();
   }
 
   clearCart() {
-    this.cartItemsSubject.next([]);
     this.cookieService.delete(CART_ITEMS_COOKIE_NAME);
-    this.syncCart();
+    this.cartItemsSubject.next([]);
+    this.calculateAllItemsCount();
   }
 
   removeItem(productId: string) {
@@ -83,19 +107,34 @@ export class OrderService {
 
     if (index > -1) {
       this.cartItemsSubject.value.splice(index, 1);
-      this.syncCart();
+      this.saveCart();
     }
   }
 
-  itemInCart(productId: string): boolean {
-    return this.cartItemsSubject.value.findIndex((o) => o.productId === productId) > -1;
+  itemInCart(productId: string): Observable<boolean> {
+    const exists = this.cartItemsSubject.value.findIndex((o) => o.productId === productId) > -1;
+
+    return of(exists);
   }
 
-  get totalPrice():number {
+  get itemsTotalPrice(): number {
     let totalPrice = 0;
     for (const item of this.cartItemsSubject.value) {
       totalPrice = totalPrice + item.unitPrice;
     }
     return totalPrice;
+  }
+
+  private calculateAllItemsCount() {
+    let count = 0;
+    for (const item of this.cartItemsSubject.value) {
+      count = count + item.count;
+    }
+    console.log('count', count);
+
+    this.itemsCountSubject.next(count);
+    console.log(this.itemsCountSubject.value);
+    console.log(this.itemsCount$);
+
   }
 }
