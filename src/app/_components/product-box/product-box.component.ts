15c3f353eb8c2847c1a49d153635da989ceb88ba
Ali M <alimousavi-01@outlook.com>
2022-03-10 16:12:15 +0330

    Implemented addToCart in productBox

--- src/app/_components/product-box/product-box.component.ts
+++ src/app/_components/product-box/product-box.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CartItemCookieModel } from '@app_models/order/cart-item-cookie';
+import { MessengerService } from '@app_services/_common/messenger/messenger.service';
 import { environment } from '@environments/environment';
 import { ProductModel } from '../../_models/shop/product/product';
+import { CartService } from '../../_services/order/cart.service';
 
 @Component({
   selector: 'app-product-box',
@@ -8,11 +11,39 @@ import { ProductModel } from '../../_models/shop/product/product';
 })
 export class ProductBoxComponent implements OnInit {
 
+  isInCart: boolean = false;
+
   @Input() product!: ProductModel;
   baseProductPath: string = environment.productBaseImagePath + '/thumbnail/';
 
-  constructor() { }
+  constructor(
+    private cartService:CartService,
+    private msg: MessengerService,
+    ) { }
+
   ngOnInit(): void {
+    this.handleCartChanges();
+  }
+
+  handleCartChanges() {
+    this.msg.getMsg().subscribe((event: any) => {
+      console.log('product change');
+
+      this.checkProductIsInCart();
+    })
+  }
+
+  checkProductIsInCart() {
+    this.cartService.itemInCart(this.product.id).subscribe((res: boolean) => {
+      this.isInCart = res;
+    })
+  }
+  addToCart() {
+    const item = new CartItemCookieModel(this.product.id, this.product.title, this.product.slug,
+      this.product.unitPrice, this.product.imagePath, 1)
+
+    this.cartService.addToCart(item);
+    this.msg.sendMsg("added item");
   }
 
 }
