dcaec6b3d37fe4a20a38db0c0028baa6258c574a
Ali M <alimousavi-01@outlook.com>
2022-03-10 15:40:13 +0330

    Created OrderModule

---
+++ src/app/pages/order/order.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '@app_components/components.module';
+import { CartService } from '@app_services/cart/cart.service';
+import { OrderRoutingModule } from './order.routing.module';
+import { CartPage } from './cart/cart.page';
+
+@NgModule({
+  declarations: [
+    CartPage
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    OrderRoutingModule,
+    ComponentsModule,
+  ],
+  exports: [
+  ],
+  schemas: [],
+  providers: [CartService]
+})
+export class OrderModule { }
