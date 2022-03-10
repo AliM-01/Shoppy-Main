c03b8fb2a0fecf75c6989242dfeb07a65a0656ee
Ali M <alimousavi-01@outlook.com>
2022-03-10 15:47:13 +0330

    Implemented computeCart service

--- src/app/_services/order/order.service.ts
+++ src/app/_services/order/order.service.ts
@@ -2,7 +2,12 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { LoadingService } from '@loading';
-import { CookieService } from 'ngx-cookie-service'
+import { IResponse } from '@app_models/_common/IResponse';
+import { Observable, throwError } from 'rxjs';
+import { CartModel } from '../../_models/order/cart';
+import { environment } from '@environments/environment';
+import { CartService } from './cart.service';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'any',
@@ -11,10 +16,28 @@ export class OrderService {
 
   constructor(
     private http: HttpClient,
-    private cookieService: CookieService,
+    private cartService: CartService,
     private toastr: ToastrService,
     private loading: LoadingService,
   ) {
   }
 
+  computeCart(): Observable<IResponse<CartModel>> {
+    this.loading.loadingOn();
+
+    const itemsData = this.cartService.getCartItems();
+
+    return this.http.post<IResponse<CartModel>>
+      (`${environment.orderBaseApiUrl}/compute-cart`, itemsData)
+      .pipe(
+        tap(() => this.loading.loadingOff()),
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+          this.loading.loadingOff();
+
+          return throwError(error);
+        })
+      );
+  }
 }
