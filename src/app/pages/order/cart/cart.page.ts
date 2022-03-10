a64010184fc27c27630b5b7ccb0da75666845239
Ali M <alimousavi-01@outlook.com>
2022-03-10 15:38:20 +0330

    Created CartPage

---
+++ src/app/pages/order/cart/cart.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-cart',
+  templateUrl: './cart.page.html'
+})
+export class CartPage implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+
+}
