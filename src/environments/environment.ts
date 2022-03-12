c4cc5edf4d2902551f2ce6c02bbb7bbf003f8648
Ali M <alimousavi-01@outlook.com>
2022-03-12 18:31:59 +0330

    added authBaseApiUrl to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  authBaseApiUrl: "https://localhost:5001/api/account",
   shopBaseApiUrl: "https://localhost:5001/api/shop",
   commentBaseApiUrl: "https://localhost:5001/api/comment",
   blogBaseApiUrl: "https://localhost:5001/api/blog",
