68b375f7af0e9aed386f51b71471e345aefa09e8
Ali M <alimousavi-01@outlook.com>
2022-03-16 15:22:32 +0330

    Created initializePaymentRequest service

--- src/app/_services/order/order.service.ts
+++ src/app/_services/order/order.service.ts
@@ -8,6 +8,8 @@ import { CartModel } from '../../_models/order/cart';
 import { environment } from '@environments/environment';
 import { CartService } from './cart.service';
 import { tap, catchError } from 'rxjs/operators';
+import { InitializePaymentRequestModel } from '@app_models/order/initialize-payment-request';
+import { InitializePaymentResponseModel } from '@app_models/order/initialize-payment-response';
 
 @Injectable({
   providedIn: 'any',
@@ -48,6 +50,23 @@ export class OrderService {
 
     return this.http.post<IResponse<CartModel>>
       (`${environment.orderBaseApiUrl}/checkout`, itemsData)
+      .pipe(
+        tap(() => this.loading.loadingOff()),
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+          this.loading.loadingOff();
+
+          return throwError(error);
+        })
+      );
+  }
+
+  initializePaymentRequest(payment: InitializePaymentRequestModel): Observable<IResponse<InitializePaymentResponseModel>> {
+    this.loading.loadingOn();
+
+    return this.http.post<IResponse<InitializePaymentResponseModel>>
+      (`${environment.orderBaseApiUrl}/initialize-payment`, payment)
       .pipe(
         tap(() => this.loading.loadingOff()),
         catchError((error: HttpErrorResponse) => {
