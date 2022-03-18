eafdd10805cf5f7ed2e92503df764585dcca69a1
Ali M <alimousavi-01@outlook.com>
2022-03-18 19:32:42 +0330

    Implemented PaymentResultPage UI

--- src/app/pages/order/payment-result/payment-result.page.ts
+++ src/app/pages/order/payment-result/payment-result.page.ts
@@ -13,6 +13,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class PaymentResultPage implements OnInit {
 
+  isVerified: boolean = false;
+  isResultSuccess: boolean = false;
+
   authority: string = '';
   orderId: string = '';
   resultMsg: string = "";
@@ -35,13 +38,19 @@ export class PaymentResultPage implements OnInit {
     this.authority = this.activatedRoute.snapshot.queryParams["authority"];
     this.orderId = this.activatedRoute.snapshot.queryParams["oId"];
 
+    console.log('authority', this.authority);
+    console.log('orderId', this.orderId);
+
     if (this.orderId === undefined || this.orderId === '') {
       this.router.navigate(['/']);
     } else {
       const status = this.activatedRoute.snapshot.queryParams["status"];
 
+      console.log('status', status);
+
       if (status !== "OK") {
         this.resultMsg = "پرداخت با موفقیت انجام نشد. درصورت کسر وجه از حساب، مبلغ تا 24 ساعت دیگر به حساب شما بازگردانده خواهد شد.";
+        this.isVerified = true;
       } else {
         this.verify();
       }
@@ -49,13 +58,24 @@ export class PaymentResultPage implements OnInit {
   }
 
   verify() {
+    this.loading.loadingOn();
+
     const payment: VerifyPaymentRequestModel = new VerifyPaymentRequestModel(this.authority, this.orderId);
 
     this.orderService.verifyPayment(payment).subscribe((res) => {
+      console.log('res', res);
+
       this.resultMsg = res.data.msg;
       this.resultIssueTracking = res.data.issueTracking;
+      this.cartService.clearCart();
+      this.isVerified = true;
+
+      this.loading.loadingOff();
+
     }, (error: HttpErrorResponse) => {
       this.resultMsg = error.error.message;
+      this.isVerified = true;
+
     })
   }
 
