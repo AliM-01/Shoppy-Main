cc892c16017819a2ddf590282e8d4753db7f6f73
Ali M <alimousavi-01@outlook.com>
2022-04-07 16:40:46 +0430

    Created SamplePage

--- src/app/pages/user-profile/user-profile.module.ts
+++ src/app/pages/user-profile/user-profile.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '@app_components/components.module';
 import { CommentModule } from '../comment/comment.module';
+import { SamplePage } from './sample/sample.page';
+import { SidebarComponent } from './side-bar/sidebar.component';
 import { UserProfileRoutingModule } from './user-profile.routing.module';
 
 @NgModule({
   declarations: [
+    SidebarComponent,
+    SamplePage
   ],
   imports: [
     CommonModule,
