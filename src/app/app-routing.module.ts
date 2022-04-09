4e9f389be2b4c1e869d48b070f513954080057b5
Ali M <alimousavi-01@outlook.com>
2022-04-09 10:57:47 +0430

    Code CleanUp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,6 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./layouts/layouts.module').then(m => m.LayoutsModule)
   },
-  {
-    path: 'user-profile',
-    loadChildren: () => import('./pages/user-profile/user-profile.module').then(m => m.UserProfileModule)
-  },
   { path: '**', pathMatch: 'full', redirectTo: 'not-found' },
 ];
 
