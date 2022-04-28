740b2d910710ae39d6398e06ea1f37793ec9c7f9
Ali M <alimousavi-01@outlook.com>
2022-04-28 17:11:21 +0430

    Implemented UserDashboardSidebarComponent

--- src/app/_components/user-dashboard-sidebar/user-dashboard-sidebar.component.ts
+++ src/app/_components/user-dashboard-sidebar/user-dashboard-sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '@app_services/account/account.service';
+import { UserProfileModel } from '../../_models/account/user-profile';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'user-dashboard-sidebar',
@@ -8,10 +10,14 @@ import { AccountService } from '@app_services/account/account.service';
 })
 export class UserDashboardSidebarComponent implements OnInit {
 
+  private userSubject: BehaviorSubject<UserProfileModel> = new BehaviorSubject<UserProfileModel>(new UserProfileModel("loading", "/assets/images/user/default-avatar.png"));
+  user$: Observable<UserProfileModel> = this.userSubject.asObservable();;
+
   constructor(
+    private accountService: AccountService
   ) { }
 
   ngOnInit(): void {
-
+    this.accountService.getProfile().subscribe((res) => this.userSubject.next(res))
   }
 }
--- src/app/_models/account/user-profile.ts
+++ src/app/_models/account/user-profile.ts
@@ -1,4 +1,8 @@
 export class UserProfileModel {
+  constructor(fullName: string, avatarBase64: string) {
+    this.fullName = fullName;
+    this.avatarBase64 = avatarBase64;
+  }
   fullName: string;
   avatarBase64: string;
 }
