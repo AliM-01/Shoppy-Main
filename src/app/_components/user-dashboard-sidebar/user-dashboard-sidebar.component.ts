23eedb2c91de90e2e208e5e511375eeb26f9ad49
Ali M <alimousavi-01@outlook.com>
2022-04-28 16:52:47 +0430

    Created AccountService

--- src/app/_components/user-dashboard-sidebar/user-dashboard-sidebar.component.ts
+++ src/app/_components/user-dashboard-sidebar/user-dashboard-sidebar.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '@app_services/account/account.service';
 
 @Component({
   selector: 'user-dashboard-sidebar',
-  templateUrl: './user-dashboard-sidebar.component.html'
+  templateUrl: './user-dashboard-sidebar.component.html',
+  providers: [AccountService]
 })
 export class UserDashboardSidebarComponent implements OnInit {
 
